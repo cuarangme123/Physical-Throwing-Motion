256f0fb820ccff5eaeb31ad906faf32acb1ce6b6
cuarangme123 <pquocanh1701@gmail.com>
2022-12-05 08:47:25 +0700

    update

--- BackEnd.py
+++ BackEnd.py
@@ -73,10 +73,12 @@ def Xien( Speed, Angle, Color ):
     temp = ( Speed * np.cos(theta) * 1 ) 
     temp = max(temp, ((Speed * np.sin(theta) * 1) - ((math.pi ** 2) * (1 ** 2) / 2)) / ScaleXY)
     temp = 2 / temp
+    cos = np.cos(theta)
+    sin = np.sin(theta)
     while ( posY >= 0 ):
         Time += temp / 2
-        posX = ( Speed * np.cos(theta) * Time ) / ScaleXY
-        posY = ((Speed * np.sin(theta) * Time) - ((math.pi ** 2) * (Time ** 2) / 2)) / ScaleXY
+        posX = ( Speed * cos * Time ) / ScaleXY
+        posY = ((Speed * sin * Time) - ((math.pi ** 2) * (Time ** 2) / 2)) / ScaleXY
         posX += BeginX
         GUI.graph.create_rectangle(posX, EndY - posY, posX, EndY - posY, outline = Color, width = 2, tags = 's')
         time.sleep(0.0005)
